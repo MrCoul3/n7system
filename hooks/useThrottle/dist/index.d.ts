c1a0b1c4adf682ad95fb37915b6d6f18c3a13c31
MrCoul3 <53871298+MrCoul3@users.noreply.github.com>
2022-04-06 21:17:51 +0300

    useThrottle hook

--- hooks/useThrottle/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import _ from "lodash";
-export declare function useThrottle(cb: (...args: string[]) => void, delay: number): _.DebouncedFunc<(...args: any) => void>;
--- hooks/useThrottle/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./n7-usethrottle.cjs.production.min.js')
-} else {
-  module.exports = require('./n7-usethrottle.cjs.development.js')
-}
